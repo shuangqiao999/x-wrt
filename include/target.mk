f733c53d6b05504ccfedb87a1c4ab00a5ad111b6
shuangqiao999 <94234866+shuangqiao999@users.noreply.github.com>
2025-08-26 16:24:13 +0800

    更新 target.mk

--- include/target.mk
+++ include/target.mk
@@ -215,7 +215,7 @@ LINUX_RECONF_DIFF = $(SCRIPT_DIR)/kconfig.pl - '>' $(call __linux_confcmd,$(filt
 ifeq ($(DUMP),1)
   BuildTarget=$(BuildTargets/DumpCurrent)
 
-  CPU_CFLAGS = -Os -pipe
+  CPU_CFLAGS = -O3 -march=x86-64-v2 -mtune=goldmont-plus -pipe
   ifneq ($(findstring mips,$(ARCH)),)
     ifneq ($(findstring mips64,$(ARCH)),)
       CPU_TYPE ?= mips64
